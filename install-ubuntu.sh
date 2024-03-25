e37e7640598d4aafcc828e8275d16fa2ec1537a0
fededav <67575679+raishid@users.noreply.github.com>
2024-03-25 04:09:47 -0400

    Initial commit

---
+++ install-ubuntu.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+#install dependencies
+sudo apt-get install -y curl wget
+
+curl -s https://packagecloud.io/install/repositories/varnishcache/varnish74/script.deb.sh | sudo bash
+
+#install varnish
+
+sudo apt-get install varnish-dev -y
+
+#add plugins modules
+
+sudo apt-get install build-essential libpcre3 libpcre3-dev libtool automake autoconf libncurses-dev pkg-config python3-docutils -y
+
+#install varnish modules
+wget https://github.com/varnish/varnish-modules/archive/refs/tags/0.23.0.zip -O /tmp/varnish-modules.zip
+
+unzip /tmp/varnish-modules.zip -d /tmp
+
+cd /tmp/varnish-modules-0.23.0 && ./bootstrap && ./configure && make && sudo make install
+
+
+
